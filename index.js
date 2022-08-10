d6dfb8c89d81d3e7bdd0dbef783b50b267fe316f
aspencass <caspenw1@gmail.com>
2022-08-10 10:02:43 -0600

    commit tree a23071d1e90121020bbc5cacd1c8594f93b1039c

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
+
+
+
+const input = document.querySelector('button');
+
 function addingEventListener() {
+  input.addEventListener('click', function(event) {
+    return "clicked"
+  });
 }
+
+
+/*
+
+const input = document.querySelector('button');
+
+function addingEventListener() {
+    input.addEventListener('click', () => alert('clicked'))
+    };
+
+    */
